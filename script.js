9c792738cabb6f24469c01d0ed4e5a92420876cf
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-09-28 22:38:26 -0300

    upgrade

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const key = "d66a70f6c2960de613aae17abe518df9";
 function ScreenData(dados){
     console.log(dados)
     document.querySelector(".city").innerHTML = dados.name;
-    document.querySelector(".wheather").outerHTML = Math.floor(dados.main.temp) + "ºC";
+    document.querySelector(".wheather").innerHTML = Math.floor(dados.main.temp) + "ºC";
     document.querySelector(".text-weather").innerHTML = dados.weather[0].description;
     document.querySelector(".humidity").innerHTML = "Umidade " + dados.main.humidity + "%";
     document.querySelector(".img").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
@@ -14,7 +14,8 @@ function ScreenData(dados){
 
 async function SearchCity(cidade){
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`)
+    .then(response => response.json());
 
     ScreenData(dados)
 
@@ -27,3 +28,4 @@ function clickSearch(){
     
     SearchCity(cidade)
 }
+
